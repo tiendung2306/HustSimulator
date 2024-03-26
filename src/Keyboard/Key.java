1bd87905b2159b0ac7834cbb29e2d548dfc3f57e
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-03-26 22:37:38 +0700

    add keyManager notes

--- src/Keyboard/Key.java
+++ src/Keyboard/Key.java
@@ -3,27 +3,27 @@ package Keyboard;
 import java.awt.event.KeyEvent;
 
 public class Key {
-    int key;
-    String keyAction;
+    int key; // W, A, S, Enter (VK_W VK_A VK_Enter)
+    String keyTag; // Up, Down, Fire, Interact
 
-    public Key(int key, String keyAction) {
+    public Key(int key, String keyTag) {
         this.key = key;
-        this.keyAction = keyAction;
+        this.keyTag = keyTag;
     }
 
     public int getKey() {
         return key;
     }
 
-    public String getKeyAction() {
-        return keyAction;
+    public String getKeyTag() {
+        return keyTag;
     }
     public void setKey(int key) {
         this.key = key;
     }
 
-    public void setKeyAction(String keyAction) {
-        this.keyAction = keyAction;
+    public void setKeyTag(String keyTag) {
+        this.keyTag = keyTag;
     }
     
 }
--- src/Keyboard/KeyboardManager.java
+++ src/Keyboard/KeyboardManager.java
@@ -1,3 +1,7 @@
+/*
+ * keyboardManager.getKey("UP") //tra ve phim w
+ */
+
 package Keyboard;
 
 import java.awt.event.KeyEvent;
@@ -6,25 +10,26 @@ import java.util.ArrayList;
 public class KeyboardManager {
     static ArrayList<Key> keys = new ArrayList<Key>();
 
-    public void init() {
+    public void init() { // tell me if you want add new key
         keys.add(new Key(KeyEvent.VK_W, "UP"));
         keys.add(new Key(KeyEvent.VK_A, "LEFT"));
         keys.add(new Key(KeyEvent.VK_S, "DOWN"));
         keys.add(new Key(KeyEvent.VK_D, "RIGHT"));
+        keys.add(new Key(KeyEvent.VK_E, "Interact"));
     }
 
-    public int getKey(String keyAction) {
+    public int getKey(String keyTag) {
         for(Key k : keys) {
-            if(k.getKeyAction() == keyAction) {
+            if(k.getKeyTag() == keyTag) {
                 return k.getKey();
             }
         }
         return -1; //chua khai bao phim nay`
     }
 
-    public void setKey(int key, String keyAction) {
+    public void setKey(int key, String keyTag) {
         for(Key k : keys) {
-            if(k.getKeyAction() == keyAction) {
+            if(k.getKeyTag() == keyTag) {
                 k.setKey(key);
                 return;
             }
