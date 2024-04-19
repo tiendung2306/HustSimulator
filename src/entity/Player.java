44fb4bf30002a05632848dea206b3108915e7645
tiendung2306 <109841830+tiendung2306@users.noreply.github.com>
2024-04-20 00:17:49 +0700

    fix static bugs

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,5 +1,6 @@
 
 package entity;
+
 import Collision.Collision;
 import main.*;
 import animation.Animation_player;
@@ -11,7 +12,8 @@ import static java.lang.Math.min;
 
 import tile.Tile;
 import tile.TileManager;
-public class Player extends Entity{
+
+public class Player extends Entity {
 
     public Animation_player animation_player_stand_RIGHT;
     GamePanel gamepanel;
@@ -30,7 +32,7 @@ public class Player extends Entity{
     public Animation_player curr_animation_player;
     TileManager tileManager;
 
-    public Player(GamePanel gamepanel, KeyHandler keyhandler, TileManager tilemanager, UI ui){
+    public Player(GamePanel gamepanel, KeyHandler keyhandler, TileManager tilemanager, UI ui) {
         this.gamepanel = gamepanel;
         this.keyhandler = keyhandler;
         this.collision = gamepanel.collision;
@@ -38,7 +40,6 @@ public class Player extends Entity{
         this.ui = ui;
         setDefaultValues();
     }
-
 
     public void setDefaultValues() {
         leftBorder = false;
@@ -49,34 +50,35 @@ public class Player extends Entity{
 
         hitArea = new Rectangle();
         boundingBox = new Rectangle();
-        boundingBox.width = 32 * gamepanel.scale;
-        boundingBox.height = 32 * gamepanel.scale;
+        boundingBox.width = 32 * GamePanel.scale;
+        boundingBox.height = 32 * GamePanel.scale;
         hitArea.x = boundingBox.width / 4;
         hitArea.y = boundingBox.height / 3 * 2;
         hitArea.width = boundingBox.width / 2;
         hitArea.height = boundingBox.height / 3;
-        screenX = gamepanel.screenWidth/2 - boundingBox.width/2;
-        screenY = gamepanel.screenHeight/2 - boundingBox.height/2;
+        screenX = GamePanel.screenWidth / 2 - boundingBox.width / 2;
+        screenY = GamePanel.screenHeight / 2 - boundingBox.height / 2;
         boundingBox.x = screenX;
         boundingBox.y = screenY;
         speed = 4;
         direction = "stand_right";
         speedSlant = 3;
 
-        animation_player_stand_RIGHT = new Animation_player(gamepanel, "res/player/character_stand_right ", 3, 0.5, boundingBox);
+        animation_player_stand_RIGHT = new Animation_player(gamepanel, "res/player/character_stand_right ", 3, 0.5,
+                boundingBox);
 
         animation_player_UP = new Animation_player(gamepanel, "res/player/character_move_up ", 4, 0.8, boundingBox);
         animation_player_DOWN = new Animation_player(gamepanel, "res/player/character_move_down ", 4, 0.8, boundingBox);
-        animation_player_RIGHT = new Animation_player(gamepanel, "res/player/character_move_right ", 4, 0.8, boundingBox);
+        animation_player_RIGHT = new Animation_player(gamepanel, "res/player/character_move_right ", 4, 0.8,
+                boundingBox);
         animation_player_LEFT = new Animation_player(gamepanel, "res/player/character_move_left ", 4, 0.8, boundingBox);
 
         curr_animation_player = animation_player_stand_RIGHT;
 
     }
 
-
-    //=============================================================================================================================================
-    public void update(){
+    // =============================================================================================================================================
+    public void update() {
         if (!Main.topGameState().equals("GamePlay"))
             return;
         int countPressed = 0;
@@ -92,7 +94,7 @@ public class Player extends Entity{
         if (countPressed > 0) {
             if (ButtonInteract)
                 ButtonInteract = false;
-            if (countPressed == 1){
+            if (countPressed == 1) {
                 if (keyhandler.upPressed) {
                     direction = "up";
                     if (!topBorder)
@@ -113,36 +115,31 @@ public class Player extends Entity{
                     if (!rightBorder)
                         newMapX += speed;
                 }
-            }
-            else {
-                if (!(keyhandler.upPressed && keyhandler.downPressed) && !(keyhandler.leftPressed && keyhandler.rightPressed))
-                {
-                    if (keyhandler.upPressed && keyhandler.leftPressed)
-                    {
+            } else {
+                if (!(keyhandler.upPressed && keyhandler.downPressed)
+                        && !(keyhandler.leftPressed && keyhandler.rightPressed)) {
+                    if (keyhandler.upPressed && keyhandler.leftPressed) {
                         direction = "up-left";
                         if (!topBorder)
                             newMapY -= speedSlant;
                         if (!leftBorder)
                             newMapX -= speedSlant;
                     }
-                    if (keyhandler.upPressed && keyhandler.rightPressed)
-                    {
+                    if (keyhandler.upPressed && keyhandler.rightPressed) {
                         direction = "up-right";
                         if (!topBorder)
                             newMapY -= speedSlant;
                         if (!rightBorder)
                             newMapX += speedSlant;
                     }
-                    if (keyhandler.downPressed && keyhandler.leftPressed)
-                    {
+                    if (keyhandler.downPressed && keyhandler.leftPressed) {
                         direction = "down-left";
                         if (!bottomBorder)
                             newMapY += speedSlant;
                         if (!leftBorder)
                             newMapX -= speedSlant;
                     }
-                    if (keyhandler.downPressed && keyhandler.rightPressed)
-                    {
+                    if (keyhandler.downPressed && keyhandler.rightPressed) {
                         direction = "down-right";
                         if (!bottomBorder)
                             newMapY += speedSlant;
@@ -157,23 +154,38 @@ public class Player extends Entity{
                 hitArea.x = newMapX + boundingBox.width / 4;
                 hitArea.y = newMapY + boundingBox.height / 3 * 2;
                 collision.scanCollision(this, gamepanel.currentMap);
-            }
-            else checkBug = true;
-            if (checkBug || collision.getNumCollision() == 0){
+            } else
+                checkBug = true;
+            if (checkBug || collision.getNumCollision() == 0) {
                 mapX = newMapX;
                 mapY = newMapY;
                 switch (direction) {
-                    case "up": curr_animation_player = animation_player_UP;break;
-                    case "up-left": curr_animation_player = animation_player_UP;break;
-                    case "down": curr_animation_player = animation_player_DOWN;break;
-                    case "down-right": curr_animation_player = animation_player_DOWN;break;
-                    case "left": curr_animation_player = animation_player_LEFT;break;
-                    case "down-left": curr_animation_player = animation_player_LEFT;break;
-                    case "up-right": curr_animation_player = animation_player_RIGHT;break;
-                    case "right": curr_animation_player = animation_player_RIGHT;break;
-                    }
-            }
-            else {
+                    case "up":
+                        curr_animation_player = animation_player_UP;
+                        break;
+                    case "up-left":
+                        curr_animation_player = animation_player_UP;
+                        break;
+                    case "down":
+                        curr_animation_player = animation_player_DOWN;
+                        break;
+                    case "down-right":
+                        curr_animation_player = animation_player_DOWN;
+                        break;
+                    case "left":
+                        curr_animation_player = animation_player_LEFT;
+                        break;
+                    case "down-left":
+                        curr_animation_player = animation_player_LEFT;
+                        break;
+                    case "up-right":
+                        curr_animation_player = animation_player_RIGHT;
+                        break;
+                    case "right":
+                        curr_animation_player = animation_player_RIGHT;
+                        break;
+                }
+            } else {
                 Tile[] tile = collision.getCollisionTile();
                 ButtonInteract = false;
                 for (int i = 0; i < collision.getNumCollision(); ++i)
@@ -182,25 +194,24 @@ public class Player extends Entity{
                         break;
                     }
             }
-        }
-        else {
+        } else {
             direction = "stand_right";
             curr_animation_player = animation_player_stand_RIGHT;
         }
         boundingBox.x = min(screenX, mapX);
         boundingBox.y = min(screenY, mapY);
-        boundingBox.x += max(0,mapX - (gamepanel.mapWidth - gamepanel.screenWidth / 2) + boundingBox.width);
-        boundingBox.y += max(0,mapY - (gamepanel.mapHeight - gamepanel.screenWidth / 2) + boundingBox.height);
+        boundingBox.x += max(0, mapX - (gamepanel.mapWidth - GamePanel.screenWidth / 2) + boundingBox.width);
+        boundingBox.y += max(0, mapY - (gamepanel.mapHeight - GamePanel.screenWidth / 2) + boundingBox.height);
         hitArea.x = mapX + boundingBox.width / 4;
         hitArea.y = mapY + boundingBox.height / 3 * 2;
         leftBorder = (boundingBox.x <= 0);
-        rightBorder = (boundingBox.x >= gamepanel.screenWidth - boundingBox.width);
+        rightBorder = (boundingBox.x >= GamePanel.screenWidth - boundingBox.width);
         topBorder = (boundingBox.y <= 0);
-        bottomBorder = (boundingBox.y >= gamepanel.screenHeight - boundingBox.height);
+        bottomBorder = (boundingBox.y >= GamePanel.screenHeight - boundingBox.height);
     }
-    public void draw(Graphics2D g2){
+
+    public void draw(Graphics2D g2) {
         tileManager.drawRect(g2, hitArea.x, hitArea.y, hitArea.width, hitArea.height);
         curr_animation_player.operation(g2);
     }
 }
-
