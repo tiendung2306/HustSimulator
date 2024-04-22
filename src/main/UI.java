21b9407a9fc0c268315125c9407369195f53ad71
ƠncủaKhải <tranmanhkhai2004@gmail.com>
2024-04-22 23:24:53 +0700

    Fix GamePanel

--- src/main/UI.java
+++ src/main/UI.java
@@ -1,7 +1,5 @@
 package main;
 
-import tile.Tile;
-
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -20,7 +18,9 @@ public class UI {
     JPanel textPanel;
     JTextArea textArea;
     BufferedImage nextIcon;
-    int iconX = 207 * gamePanel.scale, iconY = 170 * gamePanel.scale, step = 0;
+    int iconX = (int) (207 * GamePanel.scale);
+    int iconY = (int) (170 * GamePanel.scale);
+    int step = 0;
     boolean reverse;
     public UI(GamePanel gamePanel){
         this.gamePanel = gamePanel;
@@ -57,22 +57,22 @@ public class UI {
     public void drawPauseScreen(){
         String text = "PAUSED";
         int length = (int) g2.getFontMetrics().getStringBounds(text, g2).getWidth();
-        int x = (int) (gamePanel.screenWidth / 2 - length / 2);
-        int y = (int) (gamePanel.screenHeight / 2);
+        int x = (int) (GamePanel.screenWidth / 2 - length / 2);
+        int y = (int) (GamePanel.screenHeight / 2);
         g2.drawString(text, x, y);
     }
     public void drawDialogScreen(){
-        int x = 32 * (int)gamePanel.scale;
-        int width = (int) (gamePanel.screenWidth - 64 * gamePanel.scale);
-        int height = (int) (64 * gamePanel.scale);
-        int y = (int) (gamePanel.screenHeight - height - 8 * gamePanel.scale);
+        int x = 32 * (int)GamePanel.scale;
+        int width = (int) (GamePanel.screenWidth - 64 * GamePanel.scale);
+        int height = (int) (64 * GamePanel.scale);
+        int y = (int) (GamePanel.screenHeight - height - 8 * GamePanel.scale);
         int FontSize = 28;
         int FontPixel = 12;
         drawSubWindow(x,y,width,height);
 
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, FontSize));
-        x += 16 * gamePanel.scale;
-        y += 16 * gamePanel.scale;
+        x += (int) (16 * GamePanel.scale);
+        y += (int) (16 * GamePanel.scale);
         String str = "";
         int strSize = 0;
         for (int i = 0; i < currentDialog.length(); ++i){
@@ -80,7 +80,7 @@ public class UI {
             if (currentDialog.charAt(i) == ' '){
                 for (int j = i + 1; j <= currentDialog.length(); ++j)
                     if (j == currentDialog.length() || currentDialog.charAt(j) == ' '){
-                        if (strSize + (j - i - 1) * FontPixel >= width - 36 * gamePanel.scale){
+                        if (strSize + (j - i - 1) * FontPixel >= width - 36 * GamePanel.scale){
                             g2.drawString(str, x, y);
                             str = "";
                             y += 40;
@@ -96,18 +96,18 @@ public class UI {
             g2.drawString(str, x, y);
     }
     public void drawDialogueScreen(){
-        int x = 32 * (int)gamePanel.scale;
-        int width = (int) (gamePanel.screenWidth - 64 * gamePanel.scale);
-        int height = (int) (64 * gamePanel.scale);
-        int y = (int) (gamePanel.screenHeight - height - 8 * gamePanel.scale);
+        int x = 32 * (int)GamePanel.scale;
+        int width = (int) (GamePanel.screenWidth - 64 * GamePanel.scale);
+        int height = (int) (64 * GamePanel.scale);
+        int y = (int) (GamePanel.screenHeight - height - 8 * GamePanel.scale);
         int FontSize = 28;
         int FontPixel = 12;
         drawSubWindow(x,y,width,height);
 
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, FontSize));
         //timer.start();
-        x += 16 * gamePanel.scale;
-        y += 16 * gamePanel.scale;
+        x += (int) (16 * GamePanel.scale);
+        y += (int) (16 * GamePanel.scale);
         String str = "";
         int strSize = 0;
         for (int i = 0; i < currentDialog.length(); ++i){
@@ -115,7 +115,7 @@ public class UI {
             if (currentDialog.charAt(i) == ' '){
                 for (int j = i + 1; j <= currentDialog.length(); ++j)
                     if (j == currentDialog.length() || currentDialog.charAt(j) == ' '){
-                        if (strSize + (j - i - 1) * FontPixel >= width - 36 * gamePanel.scale){
+                        if (strSize + (j - i - 1) * FontPixel >= width - 36 * GamePanel.scale){
                             g2.drawString(str, x, y);
                             str = "";
                             y += 40;
@@ -132,27 +132,27 @@ public class UI {
         ++step;
         if (step == 8) {
             if (!reverse) {
-                iconX += gamePanel.scale;
-                if (iconX == 209 * gamePanel.scale) {
+                iconX += (int) GamePanel.scale;
+                if (iconX == 209 * GamePanel.scale) {
                     reverse = true;
                 }
             } else {
-                iconX -= gamePanel.scale;
-                if (iconX == 207 * gamePanel.scale) {
+                iconX -= (int) GamePanel.scale;
+                if (iconX == 207 * GamePanel.scale) {
                     reverse = false;
                 }
             }
             step = 0;
         }
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 12));
-        g2.drawString("Press Space", 183 * gamePanel.scale, 175 * gamePanel.scale);
-        g2.drawImage(nextIcon, iconX, iconY, 10 * gamePanel.scale, 7 * gamePanel.scale, null);
+        g2.drawString("Press Space", (int) (183 * GamePanel.scale), (int) (175 * GamePanel.scale));
+        g2.drawImage(nextIcon, iconX, iconY, (int)(10 * GamePanel.scale), (int)(7 * GamePanel.scale), null);
     }
     int i;
     Timer timer = new Timer(1000, new ActionListener() {
         @Override
         public void actionPerformed(ActionEvent e) {
-            char character[] = text.toCharArray();
+            char[] character = text.toCharArray();
             int arrayNumber = character.length;
 
             String addedCharacter = String.valueOf(character[i]);
@@ -171,9 +171,9 @@ public class UI {
         g2.setFont(g2.getFont().deriveFont(Font.PLAIN, 11));
         int x = gamePanel.player.getBoundingBoxX() + gamePanel.player.getBoundingBoxWidth();
         int y = gamePanel.player.getBoundingBoxY();
-        int width = (int) ((int) g2.getFontMetrics().getStringBounds(text, g2).getWidth() + 8 * gamePanel.scale);
-        int height = (int) (8 * gamePanel.scale);
-        drawSubInteractButton((int) (x - 4 * gamePanel.scale), (int) (y - height / 2 - gamePanel.scale),width,height);
+        int width = (int) ((int) g2.getFontMetrics().getStringBounds(text, g2).getWidth() + 8 * GamePanel.scale);
+        int height = (int) (8 * GamePanel.scale);
+        drawSubInteractButton((int) (x - 4 * GamePanel.scale), (int) (y - height / 2 - GamePanel.scale),width,height);
         g2.drawString(text, x, y);
     }
     public void drawSubWindow(int x, int y, int width, int height){
@@ -197,6 +197,6 @@ public class UI {
         g2.drawRoundRect(x + 2, y + 2, width - 4, height - 4, 25, 25);
     }
     public void drawBackground(BufferedImage image){
-        g2.drawImage(image, 0, 0, (int)gamePanel.screenWidth, (int)gamePanel.screenHeight, null);
+        g2.drawImage(image, 0, 0, (int)GamePanel.screenWidth, (int)GamePanel.screenHeight, null);
     }
 }
